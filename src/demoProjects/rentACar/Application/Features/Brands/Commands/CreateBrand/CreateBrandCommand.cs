ec20e98c93cff0e637c2020c5880974432d3733a
CemalTkn <cemaltaskin17@gmail.com>
2024-01-31 15:04:00 +0300

    CQRS learning and code refactoring

--- src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -15,29 +15,30 @@ namespace Application.Features.Brands.Commands.CreateBrand
     public class CreateBrandCommand :IRequest<CreatedBrandDto>
     {
         public string Name { get; set; }
+
         public class CreatedBrandCommandHandler:IRequestHandler<CreateBrandCommand, CreatedBrandDto>
         {
-           private readonly IBrandRepository _brandRepository;
-           private readonly IMapper _mapper;
+            private readonly IBrandRepository _brandRepository;
+            private readonly IMapper _mapper;
             private readonly BrandBusinessRules _brandBusinessRules;
 
             public CreatedBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
             {
-                _brandRepository = brandRepository;
-                _mapper = mapper;
-                _brandBusinessRules = brandBusinessRules;
+               _brandRepository = brandRepository;
+               _mapper = mapper;
+               _brandBusinessRules = brandBusinessRules;
             }
 
             public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
-           {
-                await _brandBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);
+            {
+               await _brandBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);//İş kuralı
 
-               Brand mappedBrand = _mapper.Map<Brand>(request);
-               Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
-               CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+               Brand mappedBrand = _mapper.Map<Brand>(request);//Kullanıcının oluşturacağı Brand ile verdiği Brand özelliğini mapliyoruz.
+               Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);//Kullanıcının verdiği Brand instancesini veritabanına ekliyoruz.
+               CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);//Oluşturlan Brand'in sadece kullanıcının görmesini istediğimiz özelliğini mapleyerek gönderiyoruz.
               
-               return createdBrandDto;
-           }
+               return createdBrandDto;//Ve Kullanıcıya çıktı olarak gösteriyoruz.
+            }
         }
 
     }
